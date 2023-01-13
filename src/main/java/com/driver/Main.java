55c175f588a957981d1b6bf9e85e84e0cc4aebc8
Chirag <chiragkath7@gmail.com>
2023-01-13 22:03:06 +0530

    Polymorphism-Solution2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,7 +7,7 @@ public class Main {
         p.product(5,5,5);
         p.product(5.0,5.0);
     }
-    static class Product{
+    public static class Product{
         public int product(int x, int y) {
             return x*y;
         }
